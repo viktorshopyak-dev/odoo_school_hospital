caf82efcfccf020da59c471e224a58b3f526c1dc
viktor.shopyak-dev <shop@monodata.online>
2026-07-07 19:41:52 +0200

    init commit

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Odoo School Library',
+    'version': '19.0.1.0.1',
+    'category': 'Customizations',
+    'summary': 'Odoo School Library',
+    'description': "Odoo School Library",
+    'website': 'https://www.odoo.school',
+    'author': 'Odoo SA',
+    'license': 'OPL-1',
+    # 'depends': [
+    #     'base',
+    # ],
+    # 'external_dependencies': {
+    #     'python': []
+    # },
+    # 'data': [],
+    # 'demo': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'images': ['static/description/icon.png'],
+}
